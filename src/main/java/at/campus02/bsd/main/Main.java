9a029728087d744f365162fe1420b496c119acce
Tihomir Yordanov <52109710@edu.campus02.at>
2022-05-22 12:28:29 +0200

    adding test log messages

--- src/main/java/at/campus02/bsd/main/Main.java
+++ src/main/java/at/campus02/bsd/main/Main.java
@@ -1,13 +1,22 @@
 package at.campus02.bsd.main;
 
 import at.campus02.bsd.Calculator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+    private final static Logger logger = LogManager.getLogger();
+
+    // Log messages
     public static void main(String[] args) {
+
+
         Calculator calculator = new Calculator();
         System.out.println(calculator.add(2,5));
+        logger.info("Welcome to calculator.");
         System.out.println(calculator.minus(2,5));
         System.out.println(calculator.divide(2,0));
+        logger.error("Error bei division!");
         System.out.println(calculator.multiply(2,5));
 
     }
